7aef28d9130b57325ef05d28b668681a72fa6046
Connie18 <Yuichiro.Kondo@sony.com>
2024-11-26 12:40:52 +0900

    Update explorer urls

--- smart-contracts/nft-hardhat/hardhat.config.ts
+++ smart-contracts/nft-hardhat/hardhat.config.ts
@@ -22,8 +22,8 @@ const config: HardhatUserConfig = {
         network: "minato",
         chainId: 1946,
         urls: {
-          apiURL: "https://explorer-testnet.soneium.org/api",
-          browserURL: "https://explorer-testnet.soneium.org",
+          apiURL: "https://soneium-minato.blockscout.com/api",
+          browserURL: "https://soneium-minato.blockscout.com",
         },
       },
     ],
